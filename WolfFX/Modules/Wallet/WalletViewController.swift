dd46d8cf7bf42a1fe3c92500bd55cd66aed624b8
Olena Ostrozhynska <o.shalena@gmail.com>
2020-04-21 14:11:08 +0300

    deposit view created

--- WolfFX/Modules/Wallet/WalletViewController.swift
+++ WolfFX/Modules/Wallet/WalletViewController.swift
@@ -11,9 +11,10 @@ import UIKit
 class WalletViewController: UIViewController, WalletViewProtocol, NavigationDesign  {
     @IBOutlet weak var segment: UISegmentedControl!
     @IBOutlet weak var depositView: UIView!
-   @IBOutlet weak var withdrawView: UIView!
- //   @IBOutlet weak var amountTextView: UITextField!
- //   @IBOutlet weak var continueButton: SubmitButton!
+    @IBOutlet weak var withdrawView: UIView!
+    @IBOutlet weak var amountTextView: UITextField!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var continueButton: SubmitButton!
     var presenter: WalletEvents?
     
     override func viewDidLoad() {
@@ -26,13 +27,13 @@ class WalletViewController: UIViewController, WalletViewProtocol, NavigationDesi
         segment.defaultConfiguration()
         depositView.isHidden = false
         withdrawView.isHidden = true
-      //  continueButton.setup(backColor: .red, borderColor: .red, text: "CONTINUE", textColor: .black)
-      //  addTextToTheLeft(textfield: amountTextView)
+        continueButton.setup(backColor: .red, borderColor: .red, text: "CONTINUE", textColor: .black)
+        addTextToTheLeft(textfield: amountTextView)
     }
     
     private func addTextToTheLeft(textfield: UITextField) {
-        let label = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: 30.0, height: 30.0))
-        label.text = "¥"
+        let label = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: 40.0, height: 30.0))
+        label.text = "  ¥"
         label.font = UIFont.systemFont(ofSize: 15)
         label.textColor = UIColor.white
         label.textAlignment = .center
